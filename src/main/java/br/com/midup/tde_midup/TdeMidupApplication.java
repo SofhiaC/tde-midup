32e0dc7ed8655e3561297b10bf931bdcfa975829
Sofh <sofhia.chromiec@gmail.com>
2025-11-05 21:51:28 -0300

    first commit

---
+++ src/main/java/br/com/midup/tde_midup/TdeMidupApplication.java
@@ -0,0 +1,13 @@
+package br.com.midup.tde_midup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdeMidupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdeMidupApplication.class, args);
+	}
+
+}
